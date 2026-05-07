45fc149035e014baa8f89b39c12a1044f650d996
thomaslum007 <thomaslum007@gmail.com>
2026-05-07 11:25:34 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,9 @@
-import { useEffect } from "react";
-
 const Index = () => {
-  useEffect(() => {
-    /*window.location.replace("/flowdesk.html");*/
-  }, []);
   return (
     <div style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center", background: "#0f0f13", color: "#e8e8f0", fontFamily: "system-ui, sans-serif" }}>
-      Loading Linearis…
+      {/* Your homepage content goes here */}
+      <h1>Welcome to Linearis</h1>
     </div>
   );
 };
-
 export default Index;
